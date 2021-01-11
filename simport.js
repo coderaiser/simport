dd5bc7d0c47f190f03e35e6be52c3cfd64bd0e8b
coderaiser <coderaiser@cloudcmd.io>
2021-01-12 00:31:34 +0200

    fix(simport) url processing

--- simport.js
+++ simport.js
@@ -6,20 +6,23 @@ const readjson = require('readjson');
 const {resolve} = require;
 const {assign} = Object;
 
-module.exports.createSimport = (url) => async (name) => {
-    let resolved = name;
+module.exports.createSimport = (url) => {
+    url = url.includes('file://') ? url : pathToFileURL(url);
     
-    if (/^\./.test(name)) {
-        url = url.includes('file://') ? url : pathToFileURL(name);
-        resolved = resolve(fileURLToPath(new URL(name, url)));
+    return async (name) => {
+        let resolved = name;
+        
+        if (/^\./.test(name)) {
+            resolved = resolve(fileURLToPath(new URL(name, url)));
+        }
+        
+        if (/\.json/.test(resolved) || /\.json/.test(resolved))
+            return await readjson(resolved);
+        
+        const imported = await import(resolved);
+        const {default: def = {}} = imported;
+        
+        return assign(def, imported);
     }
-    
-    if (/\.json/.test(resolved) || /\.json/.test(resolved))
-        return await readjson(resolved);
-    
-    const imported = await import(resolved);
-    const {default: def = {}} = imported;
-    
-    return assign(def, imported);
 };
 
